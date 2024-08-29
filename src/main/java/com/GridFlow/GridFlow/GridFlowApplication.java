69f0865ad99b811cd50f91e3f83b2b0b6288adf0
Jan Hodanek <jan.hodanek@plus4u.net>
2024-08-29 20:55:43 +0200

    initial commit

---
+++ src/main/java/com/GridFlow/GridFlow/GridFlowApplication.java
@@ -0,0 +1,13 @@
+package com.GridFlow.GridFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GridFlow/GridFlow/GridFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GridFlow.GridFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
